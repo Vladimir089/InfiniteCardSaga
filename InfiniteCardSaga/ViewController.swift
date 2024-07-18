7e26a81667007fe4acc892fa11b242eb02005839
Vladimir089 <pickerpikcer@gmail.com>
2024-07-18 15:48:43 +0300

    Initial Commit

---
+++ InfiniteCardSaga/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InfiniteCardSaga
+//
+//  Created by Владимир Кацап on 18.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
